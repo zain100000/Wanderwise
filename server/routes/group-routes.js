3f8a57319026886739ddb8fce67aab0f4bdd0cc3
Your Name <you@example.com>
2024-06-26 12:52:11 +0500

    added authorization

--- server/routes/group-routes.js
+++ server/routes/group-routes.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const groupsController = require("../controllers/group-controller");
+const authMiddleware = require("../middleware/auth/auth-middleware");
 
 // Routes
-router.post("/create", groupsController.createGroup);
-router.put("/update/:id", groupsController.updateGroup);
-router.delete("/delete/:id", groupsController.deleteGroup);
+router.post("/create", authMiddleware, groupsController.createGroup);
+router.put("/update/:id", authMiddleware, groupsController.updateGroup);
+router.delete("/delete/:id", authMiddleware, groupsController.deleteGroup);
 router.get("/get/:id", groupsController.getGroupById);
 router.get("/all", groupsController.getAllGroups);
-router.post("/join/:id", groupsController.joinGroup);
-router.post("/leave/:id", groupsController.leaveGroup);
+router.post("/join/:id", authMiddleware, groupsController.joinGroup);
+router.post("/leave/:id", authMiddleware, groupsController.leaveGroup);
 
 module.exports = router;
